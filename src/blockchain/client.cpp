a11ff645fd142641b08492d82b76e31fc3868f24
PHForge <phforge@proton.me>
2025-07-08 00:48:22 +0200

    Add empty client.cpp for BlockchainClient interface

---
+++ src/blockchain/client.cpp
@@ -0,0 +1,6 @@
+#include "client.h"
+
+namespace InariTx {
+// Empty for now, as BlockchainClient is a pure virtual interface
+// Future: Add shared utilities for blockchain clients if needed
+} // namespace InariTx
