82bad7304bd554c098f77291b8f7c3404ec8ba05
PHForge <phforge@proton.me>
2025-07-08 00:57:49 +0200

    Update main CLI to integrate Config and multithreading

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include "blockchain/bitcoin_client.h"
 #include "pdf/pdf_generator.h"
 #include "qr/qr_generator.h"
+#include "utils/config.h"
 #include <thread>
 #include <vector>
 #include <iostream>
 
 namespace InariTx {
 
-void processTransaction(const std::string& txid, Language lang) {
+void processTransaction(const std::string& txid, const Config& config) {
     try {
-        BitcoinClient client;
+        BitcoinClient client(config);
         if (!client.isValidTxid(txid)) {
             throw InvalidTxidError(txid);
         }
         TransactionData data = client.fetchTransaction(txid);
         std::string qr_svg = QRGenerator::generateQRCode(txid);
-        PDFGenerator pdf_gen(lang);
+        PDFGenerator pdf_gen(config);
         pdf_gen.createReceiptPDF(data, qr_svg);
         std::cout << "Receipt generated for TXID: " << txid << std::endl;
     } catch (const InariTxException& e) {
@@ -31,14 +32,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    Config config;
     std::string txid = argv[1];
-    InariTx::Language lang = (argc > 2 && std::string(argv[2]) == "fr") 
-        ? InariTx::Language::FR : InariTx::Language::EN;
+    if (argc > 2 && std::string(argv[2]) == "fr") {
+        config = Config(); // TODO: Set French language in Config
+    }
 
-    std::vector<std::string> txids = {txid}; // Placeholder for multiple TXIDs
+    std::vector<std::string> txids = {txid}; // Support for multiple TXIDs
     std::vector<std::thread> threads;
     for (const auto& id : txids) {
-        threads.emplace_back(InariTx::processTransaction, id, lang);
+        threads.emplace_back(InariTx::processTransaction, id, config);
     }
     for (auto& thread : threads) {
         thread.join();
